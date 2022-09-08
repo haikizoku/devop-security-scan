1bee209f2ad3d195229581dbb74ed557b881bdb4
titan-figue <luzanaperez@gmail.com>
2022-09-08 16:59:26 +0200

    async, asyncio

--- nmap3scanner/tmp/__init__.py
+++ nmap3scanner/tmp/__init__.py
@@ -4,4 +4,5 @@ from flask import Flask
 from .views import app
 from . import models
 
+# Connect sqlalchemy to app
 models.db.init_app(app)
--- nmap3scanner/tmp/models.py
+++ nmap3scanner/tmp/models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+import logging as lg   #pour la journalisation
 
 from .views import app
 
@@ -8,17 +9,25 @@ db = SQLAlchemy(app)
 class Host(db.Model):
     __tablename__ = "hosts"
     ip = db.Column(db.Text,primary_key=True)
-    timestamp = db.Column(db.Integer)
+    timeout = float
     hostname = db.Column(db.Text)
-    open = db.Column(db.Text)
+    state = db.Column(db.Text)
     filtered = db.Column(db.Text)
 
-    def __init__(self, ip, timestamp, hostname,  open, filtered):
-        self.timestamp = timestamp
+    def __init__(self, ip, timeout: float, hostname,  state, filtered):
+        self.timeout = timeout
         self.hostname = hostname
-        self.open = open
+        self.state = open
         self.filtered = filtered
 
 db.create_all  
+
+def init_db():
+  db.drop_all()
+  db.create_all()
+  db.session.add(Host("open", 1))
+  db.session.add(Host("hostname", 2))
+  db.session.commit()
+  lg.warning('Database initialized!')
 
   #  (top_port_scan, os_detection, ver_detection, tcp_scan, syn_scan)
