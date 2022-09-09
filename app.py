d14f6ad79d78e5262273169c9bc549892780e553
gregory <gnorvene@gmail.com>
2022-09-09 11:31:31 +0200

    add feature   form  in  front python

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_geek():
-    return '<h1>Hello from Flask & Docker</h2>'
 
+@app.route('/')
+def home():
+    return render_template('home.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
